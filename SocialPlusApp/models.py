7d5220bd5a880f153b2a926903d21b7240627aac
aden <adenmahad@outlook.com>
2023-06-30 05:41:21 +0300

    added some functionality and fixed some bugs

--- SocialPlusApp/models.py
+++ SocialPlusApp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.urls import reverse 
+
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    author = models.ForeignKey('auth.User',on_delete = models.CASCADE)
+    body = models.TextField()
+    created_date = models.DateTimeField( auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self): # new
+        return reverse('post_detail',args=[str(self.id)])
+    
+class Comment(models.Model):
+    author = models.ForeignKey('auth.User',on_delete = models.CASCADE,)
+    body = models.TextField()
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_date']
+
+    def __str__(self):
+        return self.body
+    
+    # def get_absolute_url(self): # new
+    #     return reverse('post_detail',args=[str(Post.id)])
--- SocialPlusApp/views.py
+++ SocialPlusApp/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from .models import Post#Comment
+from  django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+# @method_decorator(login_required,name="dispatch")
+class BlogListView(ListView):
+    
+    model = Post
+    template_name = 'home.html'
+
+# class CommentCreateView(CreateView):
+#     model = Comment
+#     fields =['body']
+#     template_name = 'post_detail.html'
+#     def form_valid(self, form):
+#         form.instance.author = self.request.user
+#         form.instance.post_id = self.kwargs['pk']
+#         return super().form_valid(form)
+    
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields =['title','author','body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields =['title','body']
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+def search(request):
+    query = request.GET.get('q')
+    if  Post.objects.filter(title__iexact=query):
+        result = Post.objects.get(title=query)
+        return render(request,"results.html",{"result":result})
+    else:
+       recommended = Post.objects.filter(title__icontains=query)
+       return render(request,"recommended_entries.html",{"recommendations":recommended})
+
